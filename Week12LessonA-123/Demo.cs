a9c6dba71375cfc332f4b23b442d512a2be090dc
Zakir <Zakir@LAPTOP-GQEFR3RC>
2017-08-01 15:29:09 -0400

    Created Demo Application

--- Week12LessonA-123/Demo.cs
+++ Week12LessonA-123/Demo.cs
@@ -7,12 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*
+ * Name: Shakil Hosin
+ * Date: August 1, 2017
+ * Description: This is a Demo application to showcase u.i
+ * version: 0.1 - Created the project
+ */
 namespace Week12LessonA_123
 {
-    public partial class Form1 : Form
+    public partial class Demo : Form
     {
-        public Form1()
+        public Demo()
         {
             InitializeComponent();
         }
--- Week12LessonA-123/Program.cs
+++ Week12LessonA-123/Program.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*
+ * Name: Shakil Hosin
+ * Date: August 1, 2017
+ * Description: This is a Demo application to showcase u.i
+ * version: 0.1 - Created the project
+ */
 namespace Week12LessonA_123
 {
     static class Program
@@ -16,7 +21,7 @@ namespace Week12LessonA_123
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Demo());
         }
     }
 }
